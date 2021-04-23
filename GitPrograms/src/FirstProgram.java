4beb437b15d69315c842f3d1b7de6c06ab269504
Win10 <Win10@DESKTOP-38O9Q3F>
2021-04-23 21:21:33 +0530

    first commit to check auto build

--- GitPrograms/src/FirstProgram.java
+++ GitPrograms/src/FirstProgram.java
@@ -2,7 +2,7 @@
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("First program");
+		System.out.println("First program. First commit to check build trigger automatically");
 
 	}
 
